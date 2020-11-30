51e38aa37f33a1efc9360abcedf150503d9b1495
Noah Hummel <info@hummelit.de>
2020-11-30 23:21:30 +0100

    Added a simple logger

---
+++ src/lib/logger.ts
@@ -0,0 +1,27 @@
+export interface StringWriter {
+    (message: string): void
+}
+
+export enum Level {
+    Info = "Info",
+    Warning = "Warning",
+    Error = "Error",
+    Fatal = "Fatal"
+}
+
+export interface Stringable {
+    toString(): string;
+}
+
+export class Logger {
+    private write: StringWriter
+
+    constructor(writer?: StringWriter) {
+        this.write = writer ?? console.log;
+    }
+
+    log(level: Level, message: Stringable): void {
+        const currentTime = new Date();
+        this.write(`[${currentTime}][${level}] ${message}`);
+    }
+}
