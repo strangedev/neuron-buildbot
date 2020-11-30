e58bf7589513e38a4269411c59f172ce2de73100
Noah Hummel <info@hummelit.de>
2020-11-30 23:25:08 +0100

    Tied config, preflight and routes together
    The new entry point is now really small and only does some setup,
    before handing off to the express router.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import { Logger, Level } from "./lib/logger";
+import { loadConfig } from "./config";
+import { loadSecrets } from "./secrets";
+import { preflight } from "./preflight";
+import { registerRoutes } from "./routes";
 
 const app = express();
 const port = 8080;
 
-// TODO register routes
-
-app.listen(port, () => {
-    console.log("Server started.");
-});
+const logger = new Logger();
+loadConfig(logger)
+    .then(async (config) => ({
+        config,
+        secrets: await loadSecrets(config, logger)
+    }))
+    .then(async ({config, secrets}) => {
+        await preflight(config, secrets, logger);
+        registerRoutes(config, secrets, logger, app);
+        app.listen(port, () => {
+            logger.log(Level.Info, "Server started.");
+        });
+    })
