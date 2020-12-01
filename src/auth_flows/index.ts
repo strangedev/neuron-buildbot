e63926b885d61277cc1eaf4ec07b3519f9b6c92b
Noah Hummel <info@hummelit.de>
2020-12-01 21:58:10 +0100

    Removed broken GitHub auth flow

--- src/auth_flows/index.ts
+++ src/auth_flows/index.ts
@@ -1,15 +1,12 @@
 import { AuthCallback } from "isomorphic-git";
 import { Config, Provider } from "../config";
 import { Secrets } from "../secrets";
-import * as GitHubFlow from "./github";
 import * as GenericFlow from "./generic";
 import { Result } from "../lib/result";
 import { AuthenticationMisconfigured } from "./flow";
 
 export function makeAuthCallback(config: Config, secrets: Secrets): Result<AuthCallback, AuthenticationMisconfigured> {
     switch (config.provider) {
-        case Provider.GitHub:
-            return GitHubFlow.makeAuthCallback(config, secrets);
         default:
             return GenericFlow.makeAuthCallback(config, secrets);
     }
