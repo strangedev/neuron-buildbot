3c65e393d3b7c1339a15a42c74ca3a507c31e4ee
Noah Hummel <info@hummelit.de>
2020-12-02 19:46:07 +0100

    Fix eslint warnings

---
+++ src/actions/buildNeuron.ts
@@ -0,0 +1,24 @@
+import { Config } from '../config';
+import { CustomError } from 'defekt';
+import { errors } from '../lib/error';
+import shell from 'shelljs';
+import { fail, okay, Result } from '../lib/result';
+
+const buildNeuron = function (config: Config): Result<string, CustomError> {
+  try {
+    const stdout = shell.exec(
+      `neuron -d ${config.localRepositoryPath} rib`,
+      {
+        silent: true
+      }
+    );
+
+    return okay(stdout.replace(/\n/ug, '\\n') || 'Nothing was written to stdout while building.');
+  } catch (ex: unknown) {
+    return fail(new errors.NeuronBuildError(undefined, { cause: ex }));
+  }
+};
+
+export {
+  buildNeuron
+};
