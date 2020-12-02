116a7623d2a9d4ed8c5bba1a5cf174c81f40be79
Noah Hummel <info@hummelit.de>
2020-12-02 20:01:51 +0100

    Fix eslint warnings

--- src/preflight.ts
+++ src/preflight.ts
@@ -1,26 +1,33 @@
-import { buildNeuron } from "./actions/build_neuron";
-import { cloneRepo } from "./actions/clone_repo";
-import { Config } from "./config";
-import { wasRepoCloned } from "./getters/repo_state";
-import { Level, Logger } from "./lib/logger";
-import { Secrets } from "./secrets";
+import { buildNeuron } from './actions/buildNeuron';
+import { cloneRepo } from './actions/cloneRepo';
+import { Config } from './config';
+import { Secrets } from './secrets';
+import { wasRepoCloned } from './getters/repo_state';
+import { Level, Logger } from './lib/logger';
 
-export async function preflight(config: Config, secrets: Secrets, logger: Logger): Promise<void> {
-    logger.log(Level.Info, "🚧 Running preflight sequence.");
-    if (! await wasRepoCloned(config)) {
-        // initial cloning of the repo
-        logger.log(Level.Info, "🚧 Repo does not seem to exist locally.");
-        logger.log(Level.Info, `📥 Cloning ${config.repositoryURL}.`);
-        (await cloneRepo(config, secrets)).unpack(error => {
-            logger.log(Level.Fatal, "🚨 Can't clone the repository!");
-            return error;
-        });
-        // build for the first time
-        buildNeuron(config).unpack(error => {
-            logger.log(Level.Fatal, "🚨 Neuron is not working!");
-            return error;
-        });
-        logger.log(Level.Info, "🔨 Built zettelkasten.");
-    }
-    logger.log(Level.Info, "🛫 Enjoy your flight!");
-}
+const preflight = async function (config: Config, secrets: Secrets, logger: Logger): Promise<void> {
+  logger.log(Level.Info, '🚧 Running preflight sequence.');
+  if (!await wasRepoCloned(config)) {
+    // Initial cloning of the repo
+    logger.log(Level.Info, '🚧 Repo does not seem to exist locally.');
+    logger.log(Level.Info, `📥 Cloning ${config.repositoryUrl}.`);
+    (await cloneRepo(config, secrets)).unpack((error): Error => {
+      logger.log(Level.Fatal, '🚨 Can\'t clone the repository!');
+
+      return error;
+    });
+
+    // Build for the first time
+    buildNeuron(config).unpack((error): Error => {
+      logger.log(Level.Fatal, '🚨 Neuron is not working!');
+
+      return error;
+    });
+    logger.log(Level.Info, '🔨 Built zettelkasten.');
+  }
+  logger.log(Level.Info, '🛫 Enjoy your flight!');
+};
+
+export {
+  preflight
+};
