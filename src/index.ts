297e8eaa4b25e7af5638905505a749b08904e159
Noah Hummel <info@hummelit.de>
2020-12-02 20:07:55 +0100

    Fix eslint warnings

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,34 @@
-import express from "express";
-import { Logger, Level } from "./lib/logger";
-import { fail } from "./lib/fail";
-import { loadConfig } from "./config";
-import { loadSecrets } from "./secrets";
-import { preflight } from "./preflight";
-import { registerRoutes } from "./routes";
+import { die } from './lib/fail';
+import express from 'express';
+import { loadConfig } from './config';
+import { preflight } from './preflight';
+import { registerRoutes } from './routes';
+import { Level, Logger } from './lib/logger';
+import { loadSecrets, Secrets } from './secrets';
 
 const logger = new Logger();
 
-loadConfig()
-    .then(async (maybeConfig) => {
-        const config = maybeConfig.unpack(error => {
-            logger.log(Level.Fatal, "🚨 Can't read config!");
-            return error;
-        });
+loadConfig().
+  then(async (configResult): Promise<void> => {
+    const config = configResult.unpack((error): Error => {
+      logger.log(Level.Fatal, '🚨 Can\'t read config!');
 
-        const secrets = (await loadSecrets(config)).unpack(error => {
-            logger.log(Level.Warning, "🔒 No secrets were provided. This will not work for private repositories.");
-            return {};
-        });
+      return error;
+    });
 
-        await preflight(config, secrets, logger);
-        
-        const app = express();
-        app.use(express.json());
-        registerRoutes(config, secrets, logger, app);
-        app.listen(config.port, () => {
-            logger.log(Level.Info, "👂 Listening for push events.");
-        });
-    }).catch(error => fail(logger, error));
+    const secrets = (await loadSecrets(config)).unpack((): Secrets => {
+      logger.log(Level.Warning, '🔒 No secrets were provided. This will not work for private repositories.');
+
+      return {};
+    });
+
+    await preflight(config, secrets, logger);
+
+    const app = express();
+
+    app.use(express.json());
+    registerRoutes(config, secrets, logger, app);
+    app.listen(config.port, (): void => {
+      logger.log(Level.Info, '👂 Listening for push events.');
+    });
+  }).catch((ex): void => die(logger, ex));
