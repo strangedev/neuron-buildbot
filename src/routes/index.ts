94c167f1840bc968295a86086d8689e958a464d2
Noah Hummel <info@hummelit.de>
2020-12-02 20:26:12 +0100

    Fix eslint warnings

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
-import { Application } from "express";
-import { config } from "process";
-import { Config } from "../config";
-import { Logger } from "../lib/logger";
-import { Secrets } from "../secrets";
-import * as onPush from "./on_push";
+import { Application } from 'express';
+import { Config } from '../config';
+import { Logger } from '../lib/logger';
+import { Secrets } from '../secrets';
+import * as onPush from './on_push';
 
-export function registerRoutes(config: Config, secrets: Secrets, logger: Logger, app: Application): void {
-    onPush.registerRoute(config, secrets, logger, app);
-}
+const registerRoutes = async function (config: Config, secrets: Secrets, logger: Logger, app: Application): Promise<void> {
+  onPush.registerRoute(config, secrets, logger, app);
+};
+
+export {
+  registerRoutes
+};
